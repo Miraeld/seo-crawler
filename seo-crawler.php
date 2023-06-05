96d0a4253c3a8f71f5114b2e1807ac20631efb5a
Gael ROBIN <grobin@cybercite.fr>
2023-06-05 16:00:11 +0200

    Initial Commit

---
+++ seo-crawler.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: SEO Crawler
+ * Description: WP Media Technical Test
+ * Version: 1.0
+ * Author: Gaël Robin
+ *
+ * This plugin allows to crawl the website to find and store its internal links.
+ */
+ */
+
+defined( 'ABSPATH' ) || exit;
